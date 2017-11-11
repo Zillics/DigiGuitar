9fedbf59b061667057c7add122d73c200bdc3806
Zillics <erik.zilliacus@gmail.com>
2017-11-11 19:56:56 +0200

    midiGuit: Classes midiSlider and midiInstrument created. Basic functionality implemented and tested

--- midiGuit.cpp
+++ midiGuit.cpp
@@ -1,17 +1,58 @@
 #define THRESH 40
 #include "midiGuit.h"
-//UNTESTED, ToDo
-/*
-void simplePlay(){
-	while(true){
-		if(readSlider(2,3) != -1){
-		MIDI.sendNoteOn(100,100,1);
-			while(readSlider(2,3) != -1){
-				sendPitchBend(map(readSlider2(0,1,10),0,2,0,65535),1);
-			}
-		MIDI.sendNoteOff(100,100,1);
-		}
-	}
-	
+//midiSlider
+midiSlider::midiSlider(int pin1,int pin2){
+	setPins(pin1,pin2);
 }
-*/
+void midiSlider::setPins(int p1,int p2){
+	pin1 = p1;
+	pin2 = p2;
+}
+
+double midiSlider::readRaw(){
+		return readSlider2(pin1,pin2,n);
+	}
+
+int midiSlider::read(){
+	double raw = readSlider2(pin1,pin2,n);
+	raw = (0.5*raw)*r;
+	return (int)raw;
+}
+
+void midiSlider::print(){
+	Serial.println("-----------SLIDER-INFO-----------");
+	Serial.print("Pins: ");
+	Serial.print(pin1);
+	Serial.print(", ");
+	Serial.println(pin2);
+	Serial.print("Number of samples n: ");
+	Serial.println(n);
+	Serial.print("Range: 0 -> ");
+	Serial.println(r);
+}
+//midiInstrument
+midiInstrument::midiInstrument(vector<midiSlider> s){
+	for(midiSlider i : s){
+		sliders.push_back(i);
+	}
+}
+
+void midiInstrument::printInfo(){
+	Serial.println("*********INSTRUMENT****INFO*********");
+	Serial.println("All sliders: ");
+	for(midiSlider i : sliders){
+		i.print();
+	}
+}
+
+void midiInstrument::addSlider(midiSlider s){
+	sliders.push_back(s);
+}
+
+void midiInstrument::printAllRead(){
+	for(midiSlider i : sliders){
+		Serial.print(i.read());
+		Serial.print(", ");
+	}
+	Serial.println();
+}
--- midiGuit.h
+++ midiGuit.h
@@ -2,11 +2,49 @@
 #define ADD_midiGH
 #include "capSlider.h"
 #include <MIDI.h>
+#include <vector>
+using std::vector;
+class midiSlider
+{
+	private:
+	//pin1 and pin2: ports used on Teensy, values: 0 - 10	
+	int pin1;
+	int pin2;
+	//n: number of samples taken when using readSlider2
+	int n = 10; 
+	//r: range of values for this slider is 0 -> r
+	int r = 255;
+	
+	public:
+	//Constructor
+	midiSlider(int p1,int p2);
+	//Trivial functions
+	void setPins(int p1,int p2);
+	void setn(int num){n = num;};
+	void setR(int maxi){r = maxi;};
+	//Function declarations
+	double readRaw();
+	int read();
+	void print();
+	//Midi functions
+	void playNote();
+	
 
+};
 
-
-
-
+class midiInstrument
+{
+	private:
+	vector<midiSlider> sliders;
+	public:
+	//Constructor
+	midiInstrument(vector<midiSlider> s);
+	//Trivial functions
+	//Function declarations
+	void printInfo();
+	void addSlider(midiSlider s);
+	void printAllRead();
+};
 
 
 
